50a200518f15e45678d244905daf0e8e52efbcec
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-09-19 14:31:04 -0400

    Initialize inventory with product objects

--- cc_5.js
+++ cc_5.js
@@ -1,1 +1,6 @@
-
+inventory = [
+    {Name: "Espresso", price: 10, quantity: 50},
+    {Name: "Cappucino", price: 7, quantity: 100},
+    {Name: "Thai Tea", price: 15, quantity: 200},
+    {Name: "Kava", price: 25, quantity: 250}
+];
