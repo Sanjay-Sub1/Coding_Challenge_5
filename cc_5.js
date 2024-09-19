217e9beba5c7255295cdc2cd1f0d80e691b24666
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-09-19 19:15:53 -0400

    Initialize orders array

--- cc_5.js
+++ cc_5.js
@@ -1,6 +1,16 @@
-inventory = [
+let inventory = [
     {Name: "Espresso", price: 10, quantity: 50},
     {Name: "Cappucino", price: 7, quantity: 100},
     {Name: "Thai Tea", price: 15, quantity: 200},
     {Name: "Kava", price: 25, quantity: 250}
 ];
+
+let orders = [];
+
+let order1 = {
+    customerName : "Sanjay",
+    [
+        {Name: "Espresso", quantity: 3}
+    ],
+    status: "Complete"
+};
